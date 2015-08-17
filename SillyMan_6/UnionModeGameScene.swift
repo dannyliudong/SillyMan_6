8e2be5547c1c8952a2977b44bb4b4af59132c688
liudong <sunmono@126.com>
2015-08-17 19:45:21 +0800

    cccc
    ccc

--- SillyMan_6/UnionModeGameScene.swift
+++ SillyMan_6/UnionModeGameScene.swift
@@ -910,6 +910,7 @@ class UnionModeGameScene: SKScene, SKPhysicsContactDelegate, UIGestureRecognizer
             
             let delayInSeconds = 0.5
             let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+            
             dispatch_after(popTime, dispatch_get_main_queue()) { () -> Void in
                 
                 self.showGameOverBottomButtons()
