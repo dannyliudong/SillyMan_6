ab7b2aabf0849528235fcb01a353c9e6a60eb9d1
liudong <sunmono@126.com>
2015-08-21 18:00:50 +0800

    cccc
    cccccc

--- SillyMan_6/GameScene.swift
+++ SillyMan_6/GameScene.swift
@@ -30,21 +30,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         Screen_Width = self.size.width
         Screen_Height = self.size.height
         
-        backgroundColor = SKColor.lightGrayColor()
-        
-        //self.physicsWorld.contactDelegate = self
+        self.physicsWorld.contactDelegate = self
         self.physicsWorld.gravity = CGVectorMake(0, -2)
 
         rootSceneNode = SKNode()
+        //rootSceneNode.position = CGPointMake(0.5, 0)
         addChild(rootSceneNode)
         
         player = SKSpriteNode (imageNamed: "submarine1")
-        player.position = CGPointMake(size.width/2, size.height/2)
-        player.physicsBody?.allowsRotation = false
-        
+        player.position = CGPointMake(150, 100)
         player.physicsBody = SKPhysicsBody(texture: player.texture, size: player.size)
         player.physicsBody?.dynamic = true
-        player.physicsBody?.angularDamping = 0.0
         
         rootSceneNode.addChild(player)
         
@@ -63,41 +59,104 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         adjustmentBackgroundPosition = self.size.width
         
-        background1 = SKSpriteNode(imageNamed: "blackStoneDown1")
-        background1.position = CGPointMake(0,  50)
-        //background1.anchorPoint = CGPointMake(0, 0.5)
+        background1 = SKSpriteNode(imageNamed: "BG1")
+        background1.anchorPoint = CGPointMake(0, 0)
+        
         background1.zPosition = -10
         rootSceneNode.addChild(background1)
         
-//        background2 = SKSpriteNode(imageNamed: "blackStoneDown")
-//        //background2.anchorPoint = CGPointMake(0, 0.5)
-//        background2.zPosition = -10
-//        background2.position = CGPointMake(background1.size.width - 1, 50);
-//        rootSceneNode.addChild(background2)
+        background2 = SKSpriteNode(imageNamed: "BG1")
+        background2.anchorPoint = CGPointMake(0, 0)
+        background2.zPosition = -10
+        background2.position = CGPointMake((background1.position.x + background1.size.width) - 1, 0);
+        rootSceneNode.addChild(background2)
         
         // 底部石头
-//        let stone1 = SKSpriteNode(imageNamed: "blackStoneDown")
-//        stone1.position = CGPointMake(Screen_Width/2 , -(Screen_Height/2 - stone1.size.height/2))
-//        background1.addChild(stone1)
-//        
-//        let stone2 = SKSpriteNode(imageNamed: "blackStoneDown")
-//        stone2.position = CGPointMake(Screen_Width/2,  -(Screen_Height/2 - stone1.size.height/2))
-//        background2.addChild(stone2)
+        let stone1 = SKSpriteNode(imageNamed: "blackStoneDown")
+        stone1.anchorPoint = CGPointMake(0.5, 0.5)
+        stone1.position = CGPointMake(0 , stone1.size.height/2)
+        background1.addChild(stone1)
+
+        let stone2 = SKSpriteNode(imageNamed: "blackStoneDown")
+        stone2.anchorPoint = CGPointMake(0.5, 0.5)
+        stone2.position = CGPointMake(0 , stone2.size.height/2)
+        background2.addChild(stone2)
         
-        background1.physicsBody = SKPhysicsBody(texture: background1.texture, size: background1.size)
-        background1.physicsBody?.dynamic = false
-        background1.physicsBody?.allowsRotation = false
-        background1.physicsBody?.affectedByGravity = true
+        stone1.physicsBody = SKPhysicsBody(texture: stone1.texture, size: stone1.size)
+        stone1.physicsBody?.angularVelocity
+        stone1.physicsBody?.dynamic = false
+        stone1.physicsBody?.allowsRotation = false
+        stone1.physicsBody?.affectedByGravity = true
         
-//        background2.physicsBody = SKPhysicsBody(texture: background2.texture, size: background2.size)
-//        background2.physicsBody?.dynamic = false
-//        background2.physicsBody?.allowsRotation = false
-//        background2.physicsBody?.affectedByGravity = true
+        stone2.physicsBody = SKPhysicsBody(texture: stone2.texture, size: stone2.size)
+        stone2.physicsBody?.dynamic = false
+        stone2.physicsBody?.allowsRotation = false
+        stone2.physicsBody?.affectedByGravity = true
         
     }
     
+    private var isone:Int = 2
+    private var odelta:CGFloat = 0
+    
+    var lastPlayerX:CGFloat = 0
+    
     //MARK: 滚动背景层
     func scrollBackground() {
+        
+        player.position.x++
+        
+        let ptX = player.position.x
+        rootSceneNode.position.x = -ptX + 150
+        
+        var scrollBG = background1
+        //var sp1 =
+        //var spaceX =  scrollBG.size.width - sp1 //scrollBG.position.x - (scrollBG.position.x - scrollBG.size.width)
+        
+        //println("spaceX :\(spaceX)")
+        
+        lastPlayerX = player.position.x
+        
+        
+        // 如果x1 >= bg1宽度 
+
+//        if spaceX >= scrollBG.size.width {
+//            
+//            background1.position.x = background2.position.x + background2.size.width
+//            scrollBG = background2
+//        }
+//        
+//        
+//        
+//        if  spaceX < background1.size.width {
+//            
+//            background1.position.x = background2.position.x + background2.size.width
+//        }
+        
+//        var delta = ptX-background1.position.x
+//        println("delta: \(delta)")
+//        if(isone == 2)
+//        {
+//            odelta = delta
+//            println("odelta :\(odelta)")
+//            isone = 0
+//        }
+//        if(delta-odelta>background1.size.width)
+//        {
+//            delta=delta-odelta
+//
+//            if(isone==0)
+//            {
+//                isone=1
+//                background1.position.x+=background2.size.width
+//            }
+//            if(isone==1)
+//            {
+//                isone=0
+//                background2.position.x+=background1.size.width
+//            }
+//        }
+        
+        
         
 //        adjustmentBackgroundPosition--
 //        
@@ -126,50 +185,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        player.physicsBody?.velocity = CGVectorMake(0, 0)
-        player.physicsBody?.applyImpulse(CGVectorMake(1, 5))
+        player.physicsBody?.applyImpulse(CGVectorMake(0, 5))
 
     }
-   
-    private var isone:Int = 2
-    private var odelta:CGFloat = 0
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
         scrollBackground()
-        
-        player.position.x++
-        let ptX = player.position.x
-        
-        //println("player.position.x : \(ptX)")
-        rootSceneNode.position.x = -ptX + 100
-        
-        var delta = ptX-background1.position.x
-        println("delta: \(delta)")
-        if(isone == 2)
-        {
-            odelta = delta
-            println("odelta :\(odelta)")
-            isone = 0
-        }
-        if(delta-odelta>background1.size.width)
-        {
-            delta=delta-odelta
-            
-            if(isone==0)
-            {
-                isone=1
-                background1.position.x+=background2.size.width
-            }
-            if(isone==1)
-            {
-                isone=0
-                background2.position.x+=background1.size.width
-            }
-        }
-        
+
         scoreLabel.text = "\(score)"
         
     }
+    
 }
--- SillyMan_6/HomeScene.swift
+++ SillyMan_6/HomeScene.swift
@@ -11,4 +11,38 @@ import SpriteKit
 
 class HomeScene: SKScene {
 
+    
+//    func login(user: String, password: String) throws {
+//    //users 是 [String: String]，存储[用户名:密码]
+//    
+//    if !users.keys.contains(user) {
+//    throw LoginError.UserNotFound
+//    }
+//    
+//    if users[user] != password {
+//    throw LoginError.UserPasswordNotMatch
+//    }
+//    
+//    print("Login successfully.")
+//    }
+//    
+//    
+//    do {
+//    try login("onevcat", password: "123")
+//    } catch LoginError.UserNotFound {
+//    print("UserNotFound")
+//    } catch LoginError.UserPasswordNotMatch {
+//    print("UserPasswordNotMatch")
+//    }
+//    
+    
 }
+
+
+
+//  异常处理
+//enum LoginError: ErrorType {
+//    case UserNotFound, UserPasswordNotMatch
+//}
+//
+
